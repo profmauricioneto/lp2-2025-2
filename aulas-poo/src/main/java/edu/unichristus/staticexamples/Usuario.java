efa2aea35e0366cf957879d8197c44df74a9aca8
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2025-10-17 07:36:22 -0300

    add: aula de atributos e métodos estáticos

---
+++ aulas-poo/src/main/java/edu/unichristus/staticexamples/Usuario.java
@@ -0,0 +1,45 @@
+package edu.unichristus.staticexamples;
+
+public class Usuario {
+    private static int idClasse = 0;
+    private int id;
+    public String nome;
+
+    public Usuario(String nome) {
+        this.nome = nome;
+        this.id = iteratorId();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    private static int iteratorId(){
+        return ++idClasse;
+    }
+
+    public static int getIdClasse() {
+        return idClasse;
+    }
+
+    public static void main(String[] args) {
+        Usuario mauricio = new Usuario("Mauricio");
+        System.out.println(mauricio.getNome() + " " +mauricio.getId());
+
+        Usuario joaozinho = new Usuario("Joaozinho");
+        System.out.println(joaozinho.getNome() + " " + joaozinho.getId());
+
+        Usuario mariazinha = new Usuario("Mariazinha");
+        System.out.println(mariazinha.getNome() + " " + mariazinha.getId());
+
+        System.out.println(Usuario.getIdClasse());
+    }
+}
